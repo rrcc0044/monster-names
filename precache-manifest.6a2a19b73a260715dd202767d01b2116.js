0c31beab131613b3507c73cd58095304908e60e7
Renz Calangi <calangi.renz@gmail.com>
2019-09-09 00:34:48 +0800

    Updates

---
+++ precache-manifest.6a2a19b73a260715dd202767d01b2116.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54d81275f754648937f297e479097add",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "26eeaf5a158ad35b9a41",
+    "url": "/monster-rolodex/static/css/main.87b6db74.chunk.css"
+  },
+  {
+    "revision": "217850eb1bffd249f5cc",
+    "url": "/monster-rolodex/static/js/2.6511e373.chunk.js"
+  },
+  {
+    "revision": "26eeaf5a158ad35b9a41",
+    "url": "/monster-rolodex/static/js/main.6f2cb3c4.chunk.js"
+  },
+  {
+    "revision": "1f1fdb189afd82863836",
+    "url": "/monster-rolodex/static/js/runtime~main.80659f76.js"
+  }
+]);
